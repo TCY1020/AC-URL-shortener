63121b9031cc83f3e9d2229491d3adfc3ba378a4
TCY1020 <a25023423@gmail.com>
2023-05-16 21:37:59 +0800

    first refer

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const exphbs = require('express-handlebars')
+
+
+const routers = require('./routes')
+const app = express()
+const port = 3000
+
+app.engine('handlebars', exphbs.engine({defaultLAyout: 'main'}))
+app.set('view engine', 'handlebars')
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(routers)
+
+app.listen(port, ()=>{
+  console.log(`URL-shortener is running on http://localhost:${port}`)
+})
---
+++ routes/index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const router = express.Router()
+
+const home = require('./modules/home.js')
+
+
+router.use('/', home)
+
+module.exports = router
